1cf5a7a3966c1ac8bd72ed257d435c30ea1c2993
uuta <yuta.morishita0916gmail.com>
2020-05-02 18:17:07 +0900

    second commit

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('category/<str:category>/', views.category, name='category'),
     path('detail/<int:blog_id>/', views.detail, name='detail'),
 ]
